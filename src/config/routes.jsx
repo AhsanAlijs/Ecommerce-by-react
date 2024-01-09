06c44a8a3446a724cbcd453417fc6c1d7bf5f531
Ahsan ali <120973691+AhsanAlijs@users.noreply.github.com>
2024-01-10 01:46:33 +0500

    Add nav bar

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -1,17 +1,23 @@
 
 import React from 'react'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../screen/home/home'
 import Products from '../screen/products/products'
+import DrawerAppBar from '../components/Navbar'
+import Contact from '../screen/Contact/Contact'
 
 const Routers = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='products' element={<Products />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <DrawerAppBar />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='products' element={<Products />} />
+                    <Route path='contact' element={<Contact />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
 
